69027cd12a2c0fbef43a258236aece59ed2c1ca2
Giuseppe Lavarello <121526026+Gaddran@users.noreply.github.com>
2024-07-25 04:15:08 -0400

    Seteo basico

---
+++ notebooks/script/01_Analisis_Exploratorio.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 
+# <div>
+# <img src="https://i.ibb.co/v3CvVz9/udd-short.png" width="150"/>
+#     <br>
+#     <strong>Universidad del Desarrollo</strong><br>
+#     <em>Magíster en Data Science</em><br>
+#     <em>Profesor: Víctor Navarro Aránguiz</em><br>
+# 
+# </div>
+# 
+# # Python para Data Science: Proyecto final
+# *15 de Julio de 2024*
+# 
+# **Fecha de Entrega**: 04 de Agosto de 2024
+# 
+# **Objetivos:**
+#   - Aplicar los conceptos aprendidos en clases.
+#   - Realizar el análisis de un dataset de su preferencia.
+#   - Familiarizarse con el manejo de versiones a través de Git.
+# 
+# **Puntaje Total:** 24 Puntos
+# 
+# **Nombre Estudiante(s)**: `Giuseppe Lavarello`
+
+
+
