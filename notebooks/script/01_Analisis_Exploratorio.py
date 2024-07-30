e4f4d332d67cf94a6b655044d624dba8de18a97e
Giuseppe Lavarello <121526026+Gaddran@users.noreply.github.com>
2024-07-30 19:28:10 -0400

    Update 01_Analisis_Exploratorio

--- notebooks/script/01_Analisis_Exploratorio.py
+++ notebooks/script/01_Analisis_Exploratorio.py
@@ -21,27 +21,56 @@
 #   - Realizar el análisis de un dataset de su preferencia.
 #   - Familiarizarse con el manejo de versiones a través de Git.
 # 
-# **Puntaje Total:** 24 Puntos
+# **Puntaje Total:** 5 Puntos
 # 
 # **Nombre Estudiante(s)**: `Giuseppe Lavarello`
 
+# ### Análisis Exploratorio
+# ##### Importe de Paquetes
+# 
+
+# Para manipulación de data 
 import numpy as np
 import pandas as pd
-import seaborn as sns
+
+# Para visualización de data
 from matplotlib import pyplot as plt
-import missingno as msno
+import seaborn as sns
+import missingno as msno #de NaNs
 
 
-df = pd.read_csv("..\\data\\raw\\winequality-white.csv", sep=";")
+
+# ##### Carga de Datos
+
+df0 = pd.read_csv("..\\data\\raw\\winequality-white.csv", sep=";")
 
 
-df.sample(10)
+# ##### Recopilación de información básica sobre los datos
+
+# Mostrar las primeras 10 filas de los datos
+
+df0.head(10)
 
 
-msno.matrix(df)
+# Mostrar 10 filas aleatorias de los datos
+
+df0.sample(10)
 
 
-df.dtypes
+# Recopilación de información básica sobre el conjunto de datos
+df0.info()
+
+
+# Recopilación de estadísticas descriptivas sobre los datos
+df0.describe()
+
+
+# Mostrar el tamaño del DataFrame
+df0.shape
+
+
+# Visualizar NaNs
+msno.matrix(df0)
 
 
 
