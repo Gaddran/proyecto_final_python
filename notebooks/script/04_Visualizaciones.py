6ab0e1402aa03c2f8da99bb0ee74b71a2da9ff4d
Giuseppe Lavarello <121526026+Gaddran@users.noreply.github.com>
2024-08-05 18:56:59 -0400

    Update de 04_Visualizaciones

--- notebooks/script/04_Visualizaciones.py
+++ notebooks/script/04_Visualizaciones.py
@@ -28,10 +28,59 @@
 # ##### Importe de Paquetes
 
 # Para manipulación de data 
+from re import S
 import numpy as np
 import pandas as pd
+import scipy
 
 # Para visualización de data
 from matplotlib import pyplot as plt
 import seaborn as sns
+
+sns.set_theme()
+
+
+df0 = pd.read_csv(r'..\data\interim\wine_inter02.csv',index_col=0)
+
+
+df0.head()
+
+
+# En el siguiente grafico podemos ver las features con minimo efecto en la variable `quality`
+
+# Se grafican el efecto de las siguientes columnas ["volatile_acidity","citric_acid","chlorides","pH","density","sulphates"]
+plt.figure(figsize=(15,7))
+
+sns.lineplot(data=df0, x="quality",y="volatile_acidity",label="Volatile Acidity")
+sns.lineplot(data=df0, x="quality",y="citric_acid",label="Citric Acid")
+sns.lineplot(data=df0, x="quality",y="chlorides",label="Chlorides")
+sns.lineplot(data=df0, x="quality",y="pH",label="pH")
+sns.lineplot(data=df0, x="quality",y="density",label="density")
+sns.lineplot(data=df0, x="quality",y="sulphates",label="Sulphates")
+
+plt.ylabel("Cantidad")
+plt.title("Impacto en quality")
+plt.legend()
+plt.show()
+
+
+plt.figure(figsize=(15,7))
+sns.lineplot(data=df0, x="quality",y="alcohol",label="Alcohol")
+plt.ylabel("Cantidad")
+plt.title("Impacto del contenido de Alcohol en quality")
+plt.legend()
+plt.show()
+
+
+def feat_plot(feature):
+    fig, ax= plt.subplots(1, 2, figsize=(16, 6))
+    sns.histplot(feature,ax=ax[0])
+    sns.boxplot(feature,ax=ax[1], orient="h")
+
+
+num_feat = df0.select_dtypes(exclude = 'object').drop(columns="quality")
+
+
+for i in num_feat.columns:
+    feat_plot(num_feat[i])
 
