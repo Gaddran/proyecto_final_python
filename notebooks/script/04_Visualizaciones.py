7c04d3eec4526215b71f490deaf2aa77f1244fc3
Giuseppe Lavarello <121526026+Gaddran@users.noreply.github.com>
2024-08-05 17:08:05 -0400

    primer update 04_Visualizaciones

---
+++ notebooks/script/04_Visualizaciones.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <div>
+# <img src="https://i.ibb.co/v3CvVz9/udd-short.png" width="150"/>
+#     <br>
+#     <strong>Universidad del Desarrollo</strong><br>
+#     <em>Magíster en Data Science</em><br>
+#     <em>Profesor: Víctor Navarro Aránguiz</em><br>
+# 
+# </div>
+# 
+# # Python para Data Science: Proyecto final
+# *15 de Julio de 2024*
+# 
+# **Fecha de Entrega**: 04 de Agosto de 2024
+# 
+# **Objetivos:**
+#   - Aplicar los conceptos aprendidos en clases.
+#   - Realizar el análisis de un dataset de su preferencia.
+#   - Familiarizarse con el manejo de versiones a través de Git.
+# 
+# **Puntaje Total:** 4 Puntos
+# 
+# **Nombre Estudiante(s)**: `Giuseppe Lavarello`
+
+# ### Visualización
+# ##### Importe de Paquetes
+
+# Para manipulación de data 
+import numpy as np
+import pandas as pd
+
+# Para visualización de data
+from matplotlib import pyplot as plt
+import seaborn as sns
+
